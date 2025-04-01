d235e3aa31ed2b557dc1f4911c142f9733d62ee5
Alston0616 <u11302237@o365.ttu.edu.tw>
2025-04-01 20:35:01 +0800

    Initial commit for HW1.c

---
+++ HW1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	printf("--------------------\n");
+	printf("|                  |\n");
+	printf("|                  |\n");
+	printf("|                  |\n");
+	printf("|     歡迎來到     |\n");
+	printf("|    E1B37的作業   |\n");
+	printf("|        **        |\n");
+	printf("|       ****       |\n");
+	printf("|      ******      |\n");
+	printf("|     ********     |\n");
+	printf("|    **********    |\n");
+	printf("|   ************   |\n");
+	printf("|  **************  |\n");
+	printf("| **************** |\n");
+	printf("|******************|\n");
+	printf("|       | |        |\n");
+	printf("|       | |        |\n");
+	printf("|       | |        |\n");
+	printf("|       | |        |\n");
+	printf("--------------------\n");
+	system("pause");
+	system("CLS");
+}
